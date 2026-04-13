be6529df05b4db4a1173c06b94f380f5ee7b29df
Beatriz Cordeiro <beatriz.fcordeiro12@gmail.com>
2026-04-13 15:49:12 -0300

    Fix: fix ident bug

--- app.py
+++ app.py
@@ -105,11 +105,11 @@ with st.form("nova_tarefa_form", clear_on_submit=True):
 with open("tarefas.json", "w") as arquivo:
    json.dump(tarefas, arquivo, indent=4) 
    
-   with st.form("nova_tarefa_form"):
+with st.form("nova_tarefa_form"):
       titulo = st.text_input("Título da tarefa")
       submitted = st.form_submit_button("Incluir")
 
-   if submitted and titulo:
+if submitted and titulo:
       nova_tarefa = {
          "titulo": titulo,
          "concluida": False
